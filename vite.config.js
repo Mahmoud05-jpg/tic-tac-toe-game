8a12ae498bc303d6ae2e019726974f0bd2914a40
Mahmoud05-jpg <174183769+Mahmoud05-jpg@users.noreply.github.com>
2025-09-13 17:28:54 -0400

    Update base path to relative for GitHub Pages and rebuild

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tic-tac-toe-game/',
+  base: './',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
